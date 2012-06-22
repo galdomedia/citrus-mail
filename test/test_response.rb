c662fdce936e9fb6189e636c89e1e9fc6279627e
Bonias <piotr@galdomedia.pl>
2012-06-22 13:56:37 +0200

    remove duplicated test

--- test/test_response.rb
+++ test/test_response.rb
@@ -54,21 +54,6 @@ class TestResponse < Test::Unit::TestCase
       end
     end
 
-    context "when freshmail response is greater than 100" do
-      setup do
-        @http_response = FakeHttpResponse.new('200', '<root><response>201</response></root>')
-      end
-      should "raise CitrusMail::CitrusMailError" do
-        exception = nil
-        begin
-          CitrusMail::Response.build_from_http_response(@http_response)
-        rescue => e
-          exception = e
-        end
-        assert exception.is_a?(CitrusMail::CitrusMailError)
-      end
-    end
-
     context "when freshmail response is lower than 100" do
       setup do
         @http_response = FakeHttpResponse.new('200', '<root><response>1</response></root>')
